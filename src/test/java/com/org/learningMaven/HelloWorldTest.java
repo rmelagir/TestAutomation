30792af4a1c2739514cac01e46670cafa8bc909e
rmelagir <rmelagir@gmail.com>
2019-03-31 23:16:57 +0530

    tc

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class HelloWorldTest {
 	public static WebDriver driver;
 	@Test
